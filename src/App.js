96de38fed66651643f48a60d11dbdb0127aef8c6
greatt8696 <greatt8696@gmail.com>
2022-10-02 11:26:58 +0900

    tailwind.config 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 function App() {
   return (
-    <div className="App w-screen h-screen bg-slate-800 grid grid-rows-3 grid-cols-3 grid-flow-col gap-4 p-5">
-      <div className="rounded-3xl bg-gradient-to-r from-type-one-blue via-type-one-puple to-type-one-yellow"></div>
-      <div className="rounded-3xl bg-gradient-to-r from-type-three-pink via-type-three-blue to-type-three-green"></div>
-      <div className="rounded-3xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></div>
-      <div className="rounded-3xl bg-gradient-to-r from-pastel-purple to-pastel-blue"></div>
-      <div className="rounded-3xl bg-gradient-to-r from-type-lime-normal to-type-lime-light"></div>
+    <div className="App object-center w-screen h-screen bg-slate-800 grid grid-rows-3 grid-cols-3 grid-flow-col gap-4 p-5">
+      <div className="blur-sm rounded-3xl bg-gradient-to-r from-type-one-blue via-type-one-puple to-type-one-yellow"></div>
+      <div className=" rounded-3xl bg-gradient-to-r from-type-three-pink via-type-three-blue to-type-three-green"></div>
+      <div className="blur-sm rounded-3xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></div>
+      <div className=" rounded-3xl bg-gradient-to-r from-pastel-purple to-pastel-blue"></div>
+      <div className="hover:animate-bounce-short blur-sm rounded-3xl bg-gradient-to-r from-type-lime-normal to-type-lime-light"></div>
+      <div className="hover:animate-wiggle rounded-3xl bg-gradient-to-r from-type-two-blue via-type-two-puple to-type-two-pink"></div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,40 @@ module.exports = {
   content: ["./src/**/*.{js, jsx, ts,tsx}"],
   theme: {
     extend: {
-      ///내가 좋아하는 색상을 지정해서 씀
-      colors:{
-        'type-one-blue' : "#4158D0",
-        'type-one-puple' : "#C850C0",
-        'type-one-yellow' : "#FFCC70",
-        'pastel-purple' : "#E0C3FC",
-        'pastel-blue' : "#8EC5FC",
-        'pastel-green-blue' : "#97D9E1",
-        'type-lime-light' : "#FFFB7D",
-        'type-lime-normal' : "#85FFBD",
-        'type-three-pink' : "#FA8BFF",
-        'type-three-blue' : "#2BD2FF",
-        'type-three-green' : "#2BFF88",
+      ///내가 원하는 색상을 지정
+      colors: {
+        "type-one-blue": "#4158D0",
+        "type-one-puple": "#C850C0",
+        "type-one-yellow": "#FFCC70",
+        "type-two-blue": "#12c2e9",
+        "type-two-puple": "#c471ed",
+        "type-two-pink": "#f64f59",
+        "type-three-pink": "#FA8BFF",
+        "type-three-blue": "#2BD2FF",
+        "type-three-green": "#2BFF88",
+        "pastel-purple": "#E0C3FC",
+        "pastel-blue": "#8EC5FC",
+        "pastel-green-blue": "#97D9E1",
+        "type-lime-light": "#FFFB7D",
+        "type-lime-normal": "#85FFBD",
+      },
+      //내가 원하는 blur 픽셀 지정
+      blur: {},
 
-      }
+      //내가 원하는 animation-설정 지정
+      animation: {
+        wiggle: "wiggle 0.2s ease-in-out infinite",
+        "bounce-short": "bounce 0.2s ease-in-out infinite",
+      },
 
+      // 내가 원하는 animation-keyframes 지정
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
     },
   },
   plugins: [],
-}
+};
